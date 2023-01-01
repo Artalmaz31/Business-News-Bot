0fe9c1d3ad5bc1b5a929414cddcec70587c0fca7
Artalmaz31 <artemalmaz@inbox.ru>
2023-01-01 20:01:00 +0300

    schedule change, bug fix

--- index.py
+++ index.py
@@ -7,73 +7,69 @@ from database import add_user, get_users, update_advice, update_news
 from utils import get_advice, get_news, get_catalog_text
 
 bot = telebot.TeleBot('5185239971:AAHiQNA37X6TlqgeDmVNPrFKzZWxHvrBe68')
-schedule = ([10, 13, 16, 19], [11, 14, 18], [12, 17], [15])
+schedule = ([11, 14, 17], [10, 13, 16], [12], [15])
 offset = datetime.timezone(datetime.timedelta(hours = 3))
 
 def send_news():
     while True:
-        news = get_news()
-        users = get_users()
-        if update_news(news[0]):
-            for id in users:
-                markup = types.InlineKeyboardMarkup()
-                url_btn = types.InlineKeyboardButton(text = 'Подробнее', url = news[1])
-                markup.add(url_btn)
-                try:
+        try:
+            news = get_news()
+            users = get_users()
+            if update_news(news[0]):
+                for id in users:
+                    markup = types.InlineKeyboardMarkup()
+                    url_btn = types.InlineKeyboardButton(text = 'Подробнее', url = news[1])
+                    markup.add(url_btn)
                     bot.send_message(id, news[0], reply_markup = markup)
-                except Exception as e:
-                    print(e)
-        time.sleep(600)
+            time.sleep(600)
+        except Exception as e:
+            print('Exception at send_news(): ', str(e))
+            time.sleep(60)
 
 def send_catalog():
     while True:
-        users = get_users()
-        time_now = datetime.datetime.now(offset).hour
-        if time_now in schedule[0]:
-            for id in []:
-                try:
+        try:
+            users = get_users()
+            time_now = datetime.datetime.now(offset).hour
+
+            if time_now in schedule[0]:
+                for id in []:
                     bot.send_message(id, get_catalog_text(0), parse_mode = 'Markdown')
-                except Exception as e:
-                    print(e)
-                time.sleep(60)
-                try:
+                    time.sleep(60)
                     bot.send_message(id, get_catalog_text(1), parse_mode = 'Markdown')
-                except Exception as e:
-                    print(e)
-        if time_now in schedule[1]:
-            for id in users:
-                try:
+
+            if time_now in schedule[1]:
+                for id in users:
                     bot.send_message(id, get_catalog_text(2), parse_mode = 'Markdown')
-                except Exception as e:
-                    print(e)
-        if time_now in schedule[2]:
-            for id in users:
-                try:
+
+            if time_now in schedule[2]:
+                for id in users:
                     bot.send_message(id, get_catalog_text(3), parse_mode = 'Markdown')
-                except Exception as e:
-                    print(e)
-        if time_now in schedule[3]:
-            for id in users:
-                try:
+
+            if time_now in schedule[3]:
+                for id in users:
                     bot.send_message(id, get_catalog_text(4), parse_mode = 'Markdown')
-                except Exception as e:
-                    print(e)
-        time.sleep(3600)
+            time.sleep(3600)
+
+        except Exception as e:
+            print('Exception at send_catalog(): ', str(e))
+            time.sleep(60)
 
 def send_advice():
     while True:
-        advice = get_advice()
-        users = get_users()
-        if update_advice(advice[0]):
-            for id in users:
-                markup = types.InlineKeyboardMarkup()
-                url_btn = types.InlineKeyboardButton(text = 'Читать далее', url = advice[2])
-                markup.add(url_btn)
-                try:
+        try:
+            advice = get_advice()
+            users = get_users()
+            if update_advice(advice[0]):
+                for id in users:
+                    markup = types.InlineKeyboardMarkup()
+                    url_btn = types.InlineKeyboardButton(text = 'Читать далее', url = advice[2])
+                    markup.add(url_btn)
                     bot.send_message(id, "*" + advice[0] + "*\n\n" + advice[1], reply_markup = markup, parse_mode = 'Markdown')
-                except Exception as e:
-                    print(e)
-        time.sleep(21600)
+            time.sleep(10800)
+        except Exception as e:
+            print('Exception at send_advice(): ', str(e))
+            time.sleep(60)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -87,48 +83,48 @@ def start(message):
         markup.add(url_btn)
         try:
             bot.send_message(id, news[0], reply_markup = markup)
-        except Exception as e:
-            print(e)
+        except Exception:
+            pass
 
 @bot.message_handler(commands=['growth_stocks'])
 def gs(message):
     id = message.chat.id
     try:
         bot.send_message(id, get_catalog_text(0), parse_mode = 'Markdown')
-    except Exception as e:
-        print(e)
+    except Exception:
+        pass
 
 @bot.message_handler(commands=['falling_stocks'])
 def fs(message):
     id = message.chat.id
     try:
         bot.send_message(id, get_catalog_text(1), parse_mode = 'Markdown')
-    except Exception as e:
-        print(e)
+    except Exception:
+        pass
 
 @bot.message_handler(commands=['exchange_rates'])
 def er(message):
     id = message.chat.id
     try:
         bot.send_message(id, get_catalog_text(2), parse_mode = 'Markdown')
-    except Exception as e:
-        print(e)
+    except Exception:
+        pass
 
 @bot.message_handler(commands=['valuable_goods'])
 def vg(message):
     id = message.chat.id
     try:
         bot.send_message(id, get_catalog_text(3), parse_mode = 'Markdown')
-    except Exception as e:
-        print(e)
+    except Exception:
+        pass
 
 @bot.message_handler(commands=['market_indexes'])
 def mi(message):
     id = message.chat.id
     try:
         bot.send_message(id, get_catalog_text(4), parse_mode = 'Markdown')
-    except Exception as e:
-        print(e)
+    except Exception:
+        pass
 
 
 Thread(target = send_news).start()
